e7a2d4e0375d4cbed7113edf9b4d8883a73c2754
Rosert2019 <tresordjonga@mail.ru>
2021-10-12 11:49:41 +0100

    Disk rent

---
+++ store/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path
+
+from . import views 
+
+
+urlpatterns = [
+    
+    path('', views.listing, name = 'listing'),
+    path('<int:album_id>', views.detail, name = 'detail'),
+    path( 'search/', views.search, name = 'search'),
+     
+    
+]
